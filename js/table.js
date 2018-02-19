65468de959a01892d40c7812f8a687c9f7cbaf1a
yuvi0049 <yuvi0049@gmail.com>
2018-02-19 14:34:55 +0530

    optimization-1

--- js/table.js
+++ js/table.js
@@ -1,57 +1,29 @@
 var tableLength = dataJson.length
+var col = [];
 
-var table = '<tr><th>URL</th><th>Issue_count</th><th>Branches</th><th>Contributor</th><th>star</th><th>commit</th><th>watcher</th></tr>';
 
-
-for( var r=0;r<tableLength;r++)
+for(var i=0;i<tableLength;i++)
 {
-	table += '<tr>';
-
-	for(var c=0;c<7;c++)
-	{
-		
-			if(c == 0){
-				table += '<td>' + dataJson[r].url + '</td>';
-
-			}
-
-			if(c == 1){
-				table += '<td>' + dataJson[r].issue_count + '</td>';
-			}
-			if(c == 2){
-				table += '<td>' + dataJson[r].branches + '</td>';
-			}
-			if(c == 3){
-				table += '<td>' + dataJson[r].contributors + '</td>';
-			}
-			if(c == 4){
-				table += '<td>' + dataJson[r].star + '</td>';
-			}
-			if(c == 5){
-				table += '<td>' + dataJson[r].commit + '</td>';
-			}
-			if(c == 6){
-				table += '<td>' + dataJson[r].watchers + '</td>';
-			}
-
-			
-		
-		
+	for(var key in dataJson[i]){
+		if (col.indexOf(key) == -1) {
+			col.push(key);
+		}
 	}
+}
 
+
+var table = '<tr>';
+for(var h=0;h<col.length;h++) {
+	table += '<th>' + col[h] + '</th>';
+}
+table += '</tr>';
+
+for( var r=0;r<tableLength;r++) {
+	table += '<tr>';
+	for(var c=0;c<7;c++) {
+		table += '<td>' + dataJson[r][col[c]] + '</td>';
+	}
 	table += '</tr>';
-	
-
-	
 }
 
 document.write('<table border=1 style="margin:auto;">' + table + '</table>');
-
-
-
-
-
-
-
-
-
